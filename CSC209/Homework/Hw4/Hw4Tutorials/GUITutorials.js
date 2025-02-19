a637e928103f24351c3f0a415598023114d3243d
susieH33 <72308708+susieH33@users.noreply.github.com>
2025-02-19 15:43:58 -0500

    repushing fixed start page

---
+++ CSC209/Homework/Hw4/Hw4Tutorials/GUITutorials.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+ var x = document.getElementById("myInput").value;
+  document.getElementById("demo").innerHTML = x;
+
+
+var y = document.getElementById("myEmail").value;
+var z = document.getElementById("myPassword").value;
+
+
+}
